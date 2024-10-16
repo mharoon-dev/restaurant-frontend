705fc5327e5620241034ca87f85f331a9ce9ba68
mharoon-dev <haroon.aghani@gmail.com>
2024-10-16 20:54:57 +0500

    add code

--- utils/url.js
+++ utils/url.js
@@ -1,2 +1,14 @@
 // export const url = "http://localhost:9000/api";
 export  const url = "https://restaurant-backend-theta.vercel.app/api";
+///
+///
+///
+///
+///
+///
+///
+///
+///
+///
+///
+///
