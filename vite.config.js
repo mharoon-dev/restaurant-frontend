5c5d51a04f80731c3b1843874fbdf557dcb032aa
mharoon-dev <haroon.aghani@gmail.com>
2024-10-16 20:51:37 +0500

    add code

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,11 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      "react-redux": require.resolve("react-redux"),
+    },
+  },
   build: {
     rollupOptions: {
       external: ["react-redux"],
