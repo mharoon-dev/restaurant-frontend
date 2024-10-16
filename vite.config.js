80d77df42cdc52b34455559d1b7617dee1a8790a
mharoon-dev <haroon.aghani@gmail.com>
2024-10-16 21:27:32 +0500

    Fixed module resolution issue and rebuilt project

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      'react-redux': 'node_modules/react-redux',
-    },
-  },
-  build: {
-    rollupOptions: {
-      external: ['react-redux'],
-    },
-  },
 });
